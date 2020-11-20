376bfb8500bbc540c9d6ab86208f6c61388e741b
Rodrigo Klein <rodrigo.klein@t2software.com.br>
2020-11-20 11:54:37 -0300

    Updated Spec to 0.1 Schema with Documentation

---
+++ v0.1/Docs/searchindex.js
@@ -0,0 +1,216 @@
+
+function CreateFileRefs()
+{
+var fr=new Array();
+            
+fr[1]=['http://hc.gs1br.org.br/', 'http___hc_gs1br_org_br_.html'];
+fr[2]=['gs1hc_v1.xsd', 'gs1hc_v1_xsd.html'];
+fr[3]=['encoding', 'encoding.html'];
+fr[4]=['version', 'version.html'];
+fr[5]=['Error', 'error.html'];
+fr[6]=['errorCode', 'errorcode.html'];
+fr[7]=['errorDescription', 'errordescription.html'];
+fr[8]=['file', 'file.html'];
+fr[9]=['encoding', 'encoding1.html'];
+fr[10]=['format', 'format.html'];
+fr[11]=['schemaVersion', 'schemaversion.html'];
+fr[12]=['type', 'type.html'];
+fr[13]=['fileContent', 'filecontent.html'];
+fr[14]=['fileURL', 'fileurl.html'];
+fr[15]=['info', 'info.html'];
+fr[16]=['key', 'key.html'];
+fr[17]=['value', 'value.html'];
+fr[18]=['infolist', 'infolist.html'];
+fr[19]=['info', 'info1.html'];
+fr[20]=['Partner', 'partner.html'];
+fr[21]=['partnerKey', 'partnerkey.html'];
+fr[22]=['partnerValue', 'partnervalue.html'];
+fr[23]=['QueryResponseItem', 'queryresponseitem.html'];
+fr[24]=['content', 'content.html'];
+fr[25]=['date', 'date.html'];
+fr[26]=['ErrorMessage', 'errormessage.html'];
+fr[27]=['date', 'date1.html'];
+fr[28]=['id', 'id.html'];
+fr[29]=['schemaVersion', 'schemaversion1.html'];
+fr[30]=['error', 'error1.html'];
+fr[31]=['InboundMessage', 'inboundmessage.html'];
+fr[32]=['date', 'date2.html'];
+fr[33]=['id', 'id1.html'];
+fr[34]=['schemaVersion', 'schemaversion2.html'];
+fr[35]=['carrier', 'carrier.html'];
+fr[36]=['content', 'content1.html'];
+fr[37]=['info', 'info2.html'];
+fr[38]=['partnerFrom', 'partnerfrom.html'];
+fr[39]=['partnerTo', 'partnerto.html'];
+fr[40]=['InboundResponse', 'inboundresponse.html'];
+fr[41]=['date', 'date3.html'];
+fr[42]=['id', 'id2.html'];
+fr[43]=['schemaVersion', 'schemaversion3.html'];
+fr[44]=['error', 'error2.html'];
+fr[45]=['receipt', 'receipt.html'];
+fr[46]=['partner', 'partner1.html'];
+fr[47]=['PreferenceData', 'preferencedata.html'];
+fr[48]=['date', 'date4.html'];
+fr[49]=['id', 'id3.html'];
+fr[50]=['schemaVersion', 'schemaversion4.html'];
+fr[51]=['data', 'data.html'];
+fr[52]=['partner', 'partner2.html'];
+fr[53]=['QueryRequest', 'queryrequest.html'];
+fr[54]=['date', 'date5.html'];
+fr[55]=['id', 'id4.html'];
+fr[56]=['schemaVersion', 'schemaversion5.html'];
+fr[57]=['partnerFrom', 'partnerfrom1.html'];
+fr[58]=['partnerTo', 'partnerto1.html'];
+fr[59]=['queryRequestItem', 'queryrequestitem.html'];
+fr[60]=['parameterKey', 'parameterkey.html'];
+fr[61]=['parameterValue', 'parametervalue.html'];
+fr[62]=['QueryResponse', 'queryresponse.html'];
+fr[63]=['date', 'date6.html'];
+fr[64]=['id', 'id5.html'];
+fr[65]=['schemaVersion', 'schemaversion6.html'];
+fr[66]=['partner', 'partner3.html'];
+fr[67]=['QueryResponseItem', 'queryresponseitem1.html'];
+fr[68]=['format', 'format1.html'];
+fr[69]=['id', 'id6.html'];
+fr[70]=['type', 'type1.html'];
+fr[71]=['UtcOnlyDateTime', 'utconlydatetime.html'];    
+ return fr;          
+}
+
+function CreateWordIndex()
+{
+var w=[
+            
+['created',[2]],
+['with',[2,31,53,68]],
+['liquid',[2]],
+['starter',[2]],
+['edition',[2]],
+['http',[2]],
+['liquid-technologies',[2]],
+['schema',[2,69]],
+['elementformdefault',[2]],
+['qualified',[2]],
+['xmlns',[2]],
+['xmlschema',[2]],
+['gs1br',[2]],
+['targetnamespace',[2]],
+['version',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]],
+['simpletype',[2,68,69,70,71]],
+['name',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]],
+['annotation',[2,5,8,15,18,20,23,26,31,40,47,53,62,68,69,70,71]],
+['documentation',[2,5,8,15,18,20,23,26,31,40,47,53,62,68,69,70,71]],
+['field',[2,5,15,18,68,69]],
+['each',[2,69,70]],
+['message',[2,26,68,69,70]],
+['inside',[2,23,62,68,69,70]],
+['restriction',[2,68,69,70,71]],
+['base',[2,68,69,70,71]],
+['string',[2,3,4,5,6,7,8,9,11,14,15,16,17,20,21,22,26,29,31,34,40,43,45,47,50,53,56,59,60,61,62,65,68,69,70]],
+['maxlength',[2,69]],
+['value',[2,15,17,19,68,69,70,71]],
+['pattern',[2,69,71]],
+['utconlydatetime',[2,23,25,26,27,31,32,40,41,47,48,53,54,62,63,71]],
+['strict',[2,71]],
+['subset',[2,71]],
+['standard',[2,31,71]],
+['datetime',[2,71]],
+['type',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,70,71]],
+['accepting',[2,71]],
+['only',[2,71]],
+['values',[2,71]],
+['conforming',[2,71]],
+['format',[2,8,10,24,36,68,71]],
+['yyyy-mm-ddthh',[2,71]],
+['content',[2,23,24,31,36,67,68]],
+['file',[2,8,23,24,31,36,68]],
+['help',[2,68]],
+['processors',[2,68]],
+['handle',[2,68]],
+['right',[2,68]],
+['transformation',[2,68]],
+['enumeration',[2,68,70]],
+['sncm',[2,68,70]],
+['epcis',[2,68,70]],
+['einvoice',[2,68]],
+['complextype',[2,5,8,15,18,20,23,26,31,40,47,53,59,62]],
+['info',[2,15,18,19,31,37,51]],
+['general',[2,15,18]],
+['purporses',[2,15,18]],
+['sequence',[2,5,8,15,18,20,23,26,31,40,47,53,59,62]],
+['element',[2,5,6,7,8,13,14,15,17,18,19,20,21,22,23,24,25,26,30,31,35,36,37,38,39,40,44,45,46,47,51,52,53,57,58,59,60,61,62,66,67]],
+['attribute',[2,3,4,8,9,10,11,12,15,16,26,27,28,29,31,32,33,34,40,41,42,43,47,48,49,50,53,54,55,56,62,63,64,65]],
+['infolist',[2,18,31,37,47,51]],
+['list',[2,18,23,53]],
+['types',[2,70]],
+['find',[2,70]],
+['envelope',[2,70]],
+['shipment',[2,70]],
+['receiving',[2,70]],
+['substitution',[2,70]],
+['revocation',[2,70]],
+['refusal',[2,70]],
+['damage',[2,70]],
+['contentdif',[2,70]],
+['unabletoreceive',[2,70]],
+['--type',[2,70]],
+['encoding',[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]],
+['carry',[2,8]],
+['information',[2,8,31,53]],
+['base64',[2,8]],
+['case',[2,8]],
+['have',[2,8]],
+['larger',[2,8]],
+['files',[2,8,53]],
+['choice',[2,8,40]],
+['fileurl',[2,8,14,24,36]],
+['filecontent',[2,8,13,24,36]],
+['base64binary',[2,8,13]],
+['schemaversion',[2,8,11,24,26,29,31,34,36,40,43,47,50,53,56,62,65]],
+['partner',[2,20,31,35,38,39,40,46,47,52,53,57,58,62,66]],
+['trading',[2,20]],
+['partnerkey',[2,20,21,35,38,39,46,52,57,58,66]],
+['partnervalue',[2,20,22,35,38,39,46,52,57,58,66]],
+['error',[2,5,26,30,40,44]],
+['errorcode',[2,5,6,30,44]],
+['errordescription',[2,5,7,30,44]],
+['queryresponseitem',[2,23,62,67]],
+['query',[2,23,53,62]],
+['response',[2,23,47,53]],
+['date',[2,23,25,26,27,31,32,40,41,47,48,53,54,62,63,67]],
+['minoccurs',[2,23,24,25,26,30,31,36,38,39,40,44,45,46,47,51,52,53,57,58,62,66,67]],
+['maxoccurs',[2,23,24,25,31,37,40,45,46,47,52,53,57,58,59,62,67]],
+['queryrequest',[2,53]],
+['request',[2,53]],
+['need',[2,31,53]],
+['send',[2,31,53]],
+['against',[2,53]],
+['webservice',[2,47,53,62]],
+['queryresponse',[2,53,62]],
+['urls',[2,53]],
+['partnerfrom',[2,31,38,53,57]],
+['partnerto',[2,31,39,53,58]],
+['unbounded',[2,53,59,62,67]],
+['queryrequestitem',[2,53,59]],
+['parameterkey',[2,53,59,60]],
+['parametervalue',[2,53,59,61]],
+['receive',[2,62]],
+['after',[2,62]],
+['method',[2,47,62]],
+['errormessage',[2,26]],
+['inboundmessage',[2,31,40]],
+['operations',[2,31]],
+['carrier',[2,31,35]],
+['inboundresponse',[2,40]],
+['result',[2,40]],
+['delivery',[2,40]],
+['receipt',[2,40,45]],
+['preferencedata',[2,47]],
+['preferences',[2,47]],
+['data',[2,47,51]],
+['utf-16',[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]],
+['optional',[8,9,10,11,12,15,16,19,24,26,27,28,29,31,32,33,34,36,40,41,42,43,47,48,49,50,53,54,55,56,62,63,64,65]]
+ ];
+ return w;
+}
+        
